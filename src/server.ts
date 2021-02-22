993567fca87975d3c87eb543dde80a32d36fef66
Mateus Queirós <mateuscqueiros1@gmail.com>
2021-02-22 16:42:51 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { response } from 'express'
+
+const app = express()
+
+app.get("/", (request, response) => {
+    return response.json({message: 'Hello world'})
+})
+
+app.post("/", (request, response) => {
+    return response.json({message: 'Os dados foram salvos'})
+})
+
+app.listen(3333, () => console.log('Server on'))
